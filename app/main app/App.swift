3b7b294481460453f44263c98194aaef6bb64978
Maxim Rysevets <root@effcore.com>
2026-02-03 12:54:26 +0300

    fix

---
+++ app/main app/App.swift
@@ -0,0 +1,16 @@
+
+/* ############################################################# */
+/* ### Copyright © 2026 Maxim Rysevets. All rights reserved. ### */
+/* ############################################################# */
+
+import SwiftUI
+
+@main struct ThisApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+
+}
