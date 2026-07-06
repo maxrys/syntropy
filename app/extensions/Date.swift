49bbbb52631d73aae40f86542608a73947a29478
Maxim Rysevets <root@effcore.com>
2026-07-06 13:02:16 +0300

    fix

--- app/extensions/Date.swift
+++ app/extensions/Date.swift
@@ -7,10 +7,14 @@ import SwiftUI
 
 extension Date {
 
+    static var now: TimeInterval {
+        Self().timeIntervalSince1970
+    }
+
     static var defaultFPS = 1.0 / 24
 
     static func spin(max: UInt, speed: Double) -> Double {
-        Double(UInt(Self().timeIntervalSince1970 * speed) % max)
+        Double(UInt(Self.now * speed) % max)
     }
 
     var formatISO8601Mono: String {
@@ -18,7 +22,7 @@ extension Date {
         formatter.dateFormat = "yyyyMMdd_HHmmss"
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        let mSec = Int(Self().timeIntervalSince1970.fractionalPart * 1_000)
+        let mSec = Int(Self.now.fractionalPart * 1_000)
         return formatter.string(from: self) + "-\(mSec)"
     }
 
--- research/extension/Numeric.swift
+++ research/extension/Numeric.swift
@@ -14,7 +14,21 @@ extension Numeric {
     }
 
     func progress(max: Self) -> Double where Self: BinaryInteger {
+        guard max  >  0   else { return 0.0 }
+        guard self >= 0   else { return 0.0 }
+        guard self <= max else { return 1.0 }
         let result = Double(self) / Double(max)
+        return result.isNaN ? 0 : result.fixBounds(
+            min: 0.0,
+            max: 1.0
+        )
+    }
+
+    func progress(min: Self, max: Self) -> Double where Self: BinaryInteger {
+        guard max  >  min else { return 0.0 }
+        guard self >= min else { return 0.0 }
+        guard self <= max else { return 1.0 }
+        let result = Double(self - min) / Double(max - min)
         return result.isNaN ? 0 : result.fixBounds(
             min: 0.0,
             max: 1.0
